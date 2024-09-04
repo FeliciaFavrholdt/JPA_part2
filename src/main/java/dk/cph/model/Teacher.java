aa60d5dee769e91c81d69dd4a94a087abfc23116
AllanChandler <cph-ac326@cphbusiness.dk>
2024-09-04 09:54:17 +0200

    Update Teacher.java

--- src/main/java/dk/cph/model/Teacher.java
+++ src/main/java/dk/cph/model/Teacher.java
@@ -10,18 +10,17 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @ToString
-@NamedQuery(name = "Teacher.deleteAll", query = "DELETE FROM Teacher")
 public class Teacher {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
-
     @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "name", nullable = false)
+    private String name;
 
     @Column(name = "zoom", unique = true)
     private String zoom;
